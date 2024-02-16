4bfd66ee2fe0d9e23eeca54b211cb38cb550ad73
Talha <mdtalha2008@gmail.com>
2024-02-16 20:59:11 +0600

    carousel add

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -3,20 +3,22 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import { Carousel } from 'react-responsive-carousel';
 const Banner = () => {
     return (
-        <Carousel>
+        <div>
+            <Carousel>
                 <div>
-                    <img src="assets/1.jpeg" />
-                    <p className="legend">Legend 1</p>
+                    <img src={img1} />
+                    
                 </div>
                 <div>
-                    <img src="assets/2.jpeg" />
-                    <p className="legend">Legend 2</p>
+                    <img src={img2} />
+                    
                 </div>
                 <div>
-                    <img src="assets/3.jpeg" />
-                    <p className="legend">Legend 3</p>
+                    <img src={img3} />
+                    
                 </div>
             </Carousel>
+        </div>
     );
 };
 
