34ea088348eefc776a7c5aa90575ad3da632f6ec
Talha <mdtalha2008@gmail.com>
2024-02-16 21:02:50 +0600

    image add

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import img1 from "../../../assets/1.webp";
+import img2 from "../../../assets/2.webp";
+import img3 from "../../../assets/3.webp";
 const Banner = () => {
     return (
         <div>
