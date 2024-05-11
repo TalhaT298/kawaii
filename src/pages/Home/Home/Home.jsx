d58f2c4404089bf2cd3ab05af7d466a74bb6bbd2
Talha <mdtalha2008@gmail.com>
2024-05-12 03:01:00 +0600

    collection add at home.jsx

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Collection from '../Collection/Collection';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <h2>This is home</h2>
+            <Collection></Collection>
         </div>
     );
 };
