e3c99093f8de3035bd8ab53706991508924b560e
Talha <mdtalha2008@gmail.com>
2024-05-12 03:09:37 +0600

    colletion h3 add

--- src/pages/Home/Collection/Collection.jsx
+++ src/pages/Home/Collection/Collection.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Collection = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <div className="mx-auto  my-4 flex justify-center items-center ">
+        <h3 className="text-3xl text-pink-400 uppercase bg-pink-100 w-full h-24 flex justify-center items-center text-center  font-bold font-serif">
+          NEW RELEASED COLLECTION
+        </h3>
+      </div>
+    </div>
+  );
 };
 
 export default Collection;
