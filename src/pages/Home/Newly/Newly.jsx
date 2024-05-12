fb814a9f776802f85a07b21e4dc34411f4167bc3
Talha <mdtalha2008@gmail.com>
2024-05-12 22:30:09 +0600

    newly add

--- src/pages/Home/Newly/Newly.jsx
+++ src/pages/Home/Newly/Newly.jsx
@@ -4,7 +4,7 @@ const Newly = () => {
   return (
     <div>
       <div className="mx-auto  flex justify-center items-center ">
-        <h3 className="text-3xl text-pink-400 uppercase bg-pink-200 w-full h-24 flex justify-center items-center text-center  font-bold font-serif">
+        <h3 className="text-3xl text-pink-400 uppercase bg-pink-100 w-full h-24 flex justify-center items-center text-center  font-bold font-serif">
         Newly released Collection!
         </h3>
       </div>
