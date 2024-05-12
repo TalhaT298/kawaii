405ccf523a4ee4ec003e400ed350a040ecedbbb8
Talha <mdtalha2008@gmail.com>
2024-05-12 22:19:02 +0600

    newly add at home.jsx

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Collection from '../Collection/Collection';
+import Newly from '../Newly/Newly';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Newly></Newly>
             <Collection></Collection>
         </div>
     );
