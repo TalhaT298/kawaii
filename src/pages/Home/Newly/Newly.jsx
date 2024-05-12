9710511511187738eea43e161ff0be8a77bad8a8
Talha <mdtalha2008@gmail.com>
2024-05-12 22:24:03 +0600

    header add

--- src/pages/Home/Newly/Newly.jsx
+++ src/pages/Home/Newly/Newly.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Newly = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <div className="mx-auto  flex justify-center items-center ">
+        <h3 className="text-3xl text-pink-400 uppercase bg-pink-200 w-full h-24 flex justify-center items-center text-center  font-bold font-serif">
+        Newly released Collection!
+        </h3>
+      </div>
+    </div>
+  );
 };
 
 export default Newly;
