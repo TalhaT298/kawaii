939392ab36b0ba156be8d328ea7dcbe2170dc087
Talha <mdtalha2008@gmail.com>
2024-05-13 18:47:29 +0600

    card add

--- src/pages/Home/Newly/Newly.jsx
+++ src/pages/Home/Newly/Newly.jsx
@@ -5,58 +5,70 @@ const Newly = () => {
     <div>
       <div className="mx-auto  flex justify-center items-center ">
         <h3 className="text-3xl text-pink-400 uppercase bg-pink-100 w-full h-24 flex justify-center items-center text-center  font-bold font-serif">
-        Newly released Collection!
+          Newly released Collection!
         </h3>
       </div>
       <div className="py-4">
         <div className="flex gap-6 justify-center items-center text-center">
-          <div
-            className=""
-            whileHover={{ scale: 1.06, transition: { duration: 0.9 } }}
-            whileTap={{ scale: 0.9 }}
-            class=" p-4"
-          >
-            <img
-              className="w-68 h-96  border-8 border-white"
-              // src={Cata}
-              alt=""
-            />
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
           </div>
-          <div
-            className=""
-            whileHover={{ scale: 1.06, transition: { duration: 0.9 } }}
-            whileTap={{ scale: 0.9 }}
-            class=" p-4"
-          >
-            <img
-              className="w-68 h-96  border-8 border-white"
-              // src={Spring}
-              alt=""
-            />
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
           </div>
-          <div
-            className=""
-            whileHover={{ scale: 1.06, transition: { duration: 0.9 } }}
-            whileTap={{ scale: 0.9 }}
-            class=" p-4"
-          >
-            <img
-              className="w-68 h-96  border-8 border-white"
-              // src={Chary}
-              alt=""
-            />
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
           </div>
-          <div
-            className=""
-            whileHover={{ scale: 1.06, transition: { duration: 0.9 } }}
-            whileTap={{ scale: 0.9 }}
-            class=" p-4"
-          >
-            <img
-              className="w-68 h-96  border-8 border-white"
-              // src={Sakura}
-              alt=""
-            />
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
           </div>
         </div>
         <button className="cursor-pointer flex justify-center items-center text-center bg-pink-300 px-6 py-3 text-white font-semibold flex-grow hover:text-pink-400 hover:bg-pink-100 mx-auto">
