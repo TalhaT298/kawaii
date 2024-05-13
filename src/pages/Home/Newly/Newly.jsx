c520596f3ea2fc8de1ec15b49ad9cad5c79078ac
Talha <mdtalha2008@gmail.com>
2024-05-13 17:58:02 +0600

    card add

--- src/pages/Home/Newly/Newly.jsx
+++ src/pages/Home/Newly/Newly.jsx
@@ -8,6 +8,61 @@ const Newly = () => {
         Newly released Collection!
         </h3>
       </div>
+      <div className="py-4">
+        <div className="flex gap-6 justify-center items-center text-center">
+          <div
+            className=""
+            whileHover={{ scale: 1.06, transition: { duration: 0.9 } }}
+            whileTap={{ scale: 0.9 }}
+            class=" p-4"
+          >
+            <img
+              className="w-68 h-96  border-8 border-white"
+              // src={Cata}
+              alt=""
+            />
+          </div>
+          <div
+            className=""
+            whileHover={{ scale: 1.06, transition: { duration: 0.9 } }}
+            whileTap={{ scale: 0.9 }}
+            class=" p-4"
+          >
+            <img
+              className="w-68 h-96  border-8 border-white"
+              // src={Spring}
+              alt=""
+            />
+          </div>
+          <div
+            className=""
+            whileHover={{ scale: 1.06, transition: { duration: 0.9 } }}
+            whileTap={{ scale: 0.9 }}
+            class=" p-4"
+          >
+            <img
+              className="w-68 h-96  border-8 border-white"
+              // src={Chary}
+              alt=""
+            />
+          </div>
+          <div
+            className=""
+            whileHover={{ scale: 1.06, transition: { duration: 0.9 } }}
+            whileTap={{ scale: 0.9 }}
+            class=" p-4"
+          >
+            <img
+              className="w-68 h-96  border-8 border-white"
+              // src={Sakura}
+              alt=""
+            />
+          </div>
+        </div>
+        <button className="cursor-pointer flex justify-center items-center text-center bg-pink-300 px-6 py-3 text-white font-semibold flex-grow hover:text-pink-400 hover:bg-pink-100 mx-auto">
+          Browse All Collections
+        </button>
+      </div>
     </div>
   );
 };
