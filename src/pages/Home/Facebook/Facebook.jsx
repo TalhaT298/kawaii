42d60d6add2032f72c92264e2d97593e3e4e7cd7
Talha <mdtalha2008@gmail.com>
2024-05-15 11:03:55 +0600

    follwo us on facebook add

--- src/pages/Home/Facebook/Facebook.jsx
+++ src/pages/Home/Facebook/Facebook.jsx
@@ -5,7 +5,9 @@ const Facebook = () => {
         <div>
             <div className="mx-auto  flex justify-center items-center ">
         <h3 className="text-3xl text-pink-400 uppercase bg-pink-100 w-full h-24 flex justify-center items-center text-center  font-bold font-serif">
-          Follow us on facebook 
+          Follow us on facebook <span className="bg-pink-300 ml-2 px-6 w-68 px-1  py-2 text-white font-semibold  hover:text-pink-400 hover:bg-pink-100">
+            @Kawai Velvet
+          </span>
         </h3>
       </div>
         </div>
