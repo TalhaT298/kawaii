8aa839f10374532eb8070612d8fc367233937efd
Talha <mdtalha2008@gmail.com>
2024-05-15 23:54:26 +0600

    name update

--- src/pages/Home/Facebook/Facebook.jsx
+++ src/pages/Home/Facebook/Facebook.jsx
@@ -7,7 +7,7 @@ const Facebook = () => {
         <h3 className="text-3xl text-pink-400 uppercase bg-pink-100 w-full h-24 flex justify-center items-center text-center  font-bold font-serif">
           Follow us on facebook{" "}
           <span className="bg-pink-300 ml-2 px-6 w-68 px-1  py-1 text-white font-semibold  hover:text-pink-400 hover:bg-pink-100">
-            @Kawai Velvet
+            @Kawai Velve
           </span>
         </h3>
       </div>
