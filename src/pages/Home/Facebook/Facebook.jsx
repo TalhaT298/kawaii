f5d326ce4312ca55fc362ae4eccd116f0ceaa76d
Talha <mdtalha2008@gmail.com>
2024-05-15 10:51:50 +0600

    follow us on add

--- src/pages/Home/Facebook/Facebook.jsx
+++ src/pages/Home/Facebook/Facebook.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const Facebook = () => {
     return (
         <div>
-            
+            <div className="mx-auto  flex justify-center items-center ">
+        <h3 className="text-3xl text-pink-400 uppercase bg-pink-100 w-full h-24 flex justify-center items-center text-center  font-bold font-serif">
+          Follow us on facebook 
+        </h3>
+      </div>
         </div>
     );
 };
