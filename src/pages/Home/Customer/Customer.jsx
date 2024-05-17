26e3516c4ca7a6ee3abfc9958568913479bcabfa
Talha <mdtalha2008@gmail.com>
2024-05-17 18:02:56 +0600

    client feedback add

--- src/pages/Home/Customer/Customer.jsx
+++ src/pages/Home/Customer/Customer.jsx
@@ -4,7 +4,11 @@ import React from 'react';
 const Customer = () => {
     return (
         <div>
-            
+            <div className="mx-auto  flex justify-center items-center ">
+        <h3 className="text-3xl text-gray-50 uppercase bg-pink-300 w-full h-24 flex justify-center items-center text-center  font-bold font-serif">
+        Client Feedback
+        </h3>
+      </div>
         </div>
     );
 };
