2b4aa58fa01a5a4faff662f6b9f7eefb6071fea7
Talha <mdtalha2008@gmail.com>
2024-05-17 14:07:23 +0600

    customer add at home.jsx

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Collection from '../Collection/Collection';
 import Newly from '../Newly/Newly';
 import Facebook from '../Facebook/Facebook';
+import Customer from '../Customer/Customer';
 
 const Home = () => {
     return (
@@ -11,6 +13,7 @@ const Home = () => {
             <Newly></Newly>
             <Collection></Collection>
             <Facebook></Facebook>
+            <Customer></Customer>
         </div>
     );
 };
