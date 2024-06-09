864d2f6e6e71fdabd54883746aace311729c1c80
Talha <mdtalha2008@gmail.com>
2024-06-10 01:41:53 +0600

    customer update

--- src/pages/Home/Customer/Customer.jsx
+++ src/pages/Home/Customer/Customer.jsx
@@ -1,16 +1,41 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import required modules
+import { Pagination, Navigation } from "swiper/modules";
 
 const Customer = () => {
-    return (
-        <div>
-            <div className='className="mx-auto  flex justify-center items-center "'>
-            <h3 className="text-3xl text-gray-50 uppercase bg-pink-300 w-full h-24 flex justify-center items-center text-center  font-bold font-serif">
-        Client Feedback
-      </h3>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className='mx-auto flex justify-center items-center'>
+        <h3 className="text-3xl text-gray-50 uppercase bg-pink-300 w-full h-24 flex justify-center items-center text-center font-bold font-serif">
+          Client Feedback
+        </h3>
+      </div>
+      <div>
+        <Swiper
+          pagination={{
+            type: 'progressbar',
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className="mySwiper "
+        >
+          <SwiperSlide><p className="">ok</p></SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
 };
 
 export default Customer;
