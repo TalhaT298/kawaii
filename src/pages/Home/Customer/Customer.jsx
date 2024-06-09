00d2bd3eb37ceee96244357351bcb996ab8d2842
Talha <mdtalha2008@gmail.com>
2024-06-10 02:51:00 +0600

    slider update

--- src/pages/Home/Customer/Customer.jsx
+++ src/pages/Home/Customer/Customer.jsx
@@ -15,24 +15,26 @@ const Customer = () => {
         </h3>
       </div>
       <div>
-        <Swiper
-          pagination={{
-            type: 'progressbar',
-          }}
-          navigation={true}
-          modules={[Pagination, Navigation]}
-          className="mySwiper "
-        >
-          <SwiperSlide><p className="">ok</p></SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
-          <SwiperSlide>Slide 6</SwiperSlide>
-          <SwiperSlide>Slide 7</SwiperSlide>
-          <SwiperSlide>Slide 8</SwiperSlide>
-          <SwiperSlide>Slide 9</SwiperSlide>
-        </Swiper>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
       </div>
     </div>
   );
