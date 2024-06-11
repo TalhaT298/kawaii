d9f16630d9f8d6732501640ab141d69e0942363b
Talha <mdtalha2008@gmail.com>
2024-06-12 02:28:49 +0600

    4 customer add

--- src/pages/Home/Customer/Customer.jsx
+++ src/pages/Home/Customer/Customer.jsx
@@ -108,7 +108,32 @@ const Customer = () => {
                 ))}
               </div>
             </div></SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide><div className="flex flex-col items-center p-6 bg-white border-8 border-pink-300 shadow-md">
+              <img
+                src={Razia} // Replace with actual image path
+                alt="Customer"
+                className="w-24 h-24 rounded-full object-cover mb-4"
+              />
+              <h3 className="text-xl font-semibold text-pink-300 mb-2">
+                Razia Sultana
+              </h3>
+              <p className="text-gray-700 text-center mb-4">
+                The customer service is excellent! They are always ready to help
+                with any queries and the return policy is hassle-free.I love how
+                reliable this Velvet is
+              </p>
+              <div className="flex space-x-1 text-yellow-500">
+                {[...Array(5)].map((_, index) => (
+                  <svg
+                    key={index}
+                    className="w-6 h-6 fill-current"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 .587l3.668 7.429L24 9.234l-5.686 5.423L19.747 24 12 20.413 4.253 24l1.433-9.343L0 9.234l8.332-1.218z" />
+                  </svg>
+                ))}
+              </div>
+            </div></SwiperSlide>
           <SwiperSlide>Slide 5</SwiperSlide>
           <SwiperSlide>Slide 6</SwiperSlide>
           <SwiperSlide>Slide 7</SwiperSlide>
