2511c805e470bb3f4a4e634bf9cde1e9437a364a
Talha <mdtalha2008@gmail.com>
2024-06-12 02:43:31 +0600

    Zara add

--- src/pages/Home/Customer/Customer.jsx
+++ src/pages/Home/Customer/Customer.jsx
@@ -164,7 +164,30 @@ const Customer = () => {
                 ))}
               </div>
             </div></SwiperSlide>
-          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide><div className="flex flex-col items-center p-6 bg-white border-8 border-pink-300 shadow-md">
+              <img
+                src={Zara} // Replace with actual image path
+                alt="Customer"
+                className="w-24 h-24 rounded-full object-cover mb-4"
+              />
+              <h3 className="text-xl font-semibold text-pink-300 mb-2">
+              Zara Hossain
+              </h3>
+              <p className="text-gray-700 text-center mb-4">
+              I appreciate the easy navigation and user-friendly interface of the website. It makes shopping a breeze, even for someone like me who is not very tech-savvy
+              </p>
+              <div className="flex space-x-1 text-yellow-500">
+                {[...Array(5)].map((_, index) => (
+                  <svg
+                    key={index}
+                    className="w-6 h-6 fill-current"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 .587l3.668 7.429L24 9.234l-5.686 5.423L19.747 24 12 20.413 4.253 24l1.433-9.343L0 9.234l8.332-1.218z" />
+                  </svg>
+                ))}
+              </div>
+            </div></SwiperSlide>
           <SwiperSlide>Slide 7</SwiperSlide>
           <SwiperSlide>Slide 8</SwiperSlide>
           <SwiperSlide>Slide 9</SwiperSlide>
