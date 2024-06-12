28922debbaceaf0c0e91a48f6acdbb943e7f7588
Talha <mdtalha2008@gmail.com>
2024-06-13 01:13:33 +0600

    shared add

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Services = () => {
     return (
         <div>
-            
+            <Link to="/services"></Link>
         </div>
     );
 };
