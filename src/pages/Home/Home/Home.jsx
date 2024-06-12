af311b94746e953683feb40b29d1a06c4bc7513f
Talha <mdtalha2008@gmail.com>
2024-06-12 21:45:13 +0600

    off at add home.jsx

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -5,10 +5,12 @@ import Collection from '../Collection/Collection';
 import Newly from '../Newly/Newly';
 import Facebook from '../Facebook/Facebook';
 import Customer from '../Customer/Customer';
+import Off from '../Off/Off';
 
 const Home = () => {
     return (
         <div>
+            <Off></Off>
             <Banner></Banner>
             <Newly></Newly>
             <Collection></Collection>
