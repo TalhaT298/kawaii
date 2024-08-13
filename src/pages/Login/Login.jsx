3c0dd3b497a1222890b5ed7aab79f7d7bd4b819b
Talha <mdtalha2008@gmail.com>
2024-08-14 00:43:27 +0600

    app export at login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,10 +2,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { getAuth } from "firebase/auth";
+import app from '../../firebase/firebase.init';
 
 
 const Login = () => {
-    const auth = getAuth()
+    const auth = getAuth(app)
     return (
         <>
            <Link to="/login"></Link>
