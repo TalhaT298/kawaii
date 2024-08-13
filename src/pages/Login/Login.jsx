f47a028589acc1e02dbb770c6f02e286f9c9cffe
Talha <mdtalha2008@gmail.com>
2024-08-14 00:49:56 +0600

    google authprovider add

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,15 +3,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { getAuth } from "firebase/auth";
 import app from '../../firebase/firebase.init';
+import { GoogleAuthProvider } from 'firebase/auth/web-extension';
 
 
 const Login = () => {
-    const auth = getAuth(app)
+    const auth = getAuth(app);
+    const provider = new GoogleAuthProvider();
+
+    const handleGoogleSignIn = () =>{
+        console.log('Google is coming')
+    }
     return (
         <>
            <Link to="/login"></Link>
            <div>
-            <button>Go to Login</button>
+            <button onClick={handleGoogleSignIn}>Go to Login</button>
             </div> 
         </>
     );
