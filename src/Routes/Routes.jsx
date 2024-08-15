ccee47d8a49464366215e5c044c4b62e42a48074
Talha <mdtalha2008@gmail.com>
2024-08-16 00:40:43 +0600

    register add at routrt

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -6,6 +6,7 @@ import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Services from "../pages/Services/Services";
 import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
 
  export const router = createBrowserRouter([
     {
@@ -23,6 +24,10 @@ import Login from "../pages/Login/Login";
         {
           path: "/login",
           element: <Login></Login>
+        },
+        {
+          path: "/register",
+          element: <Register></Register>
         },
 
       ]
