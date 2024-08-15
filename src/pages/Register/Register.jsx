3cd5e14adcbb1167e2b0329c760ddee57cb494f4
Talha <mdtalha2008@gmail.com>
2024-08-16 00:52:33 +0600

    register

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -4,7 +4,14 @@ import React from 'react';
 const Register = () => {
     return (
         <div>
-            
+            <h2 className='text-3xl'>Please Register</h2>
+            <form action="">
+                <input type="email" name='email' id='' />
+                <br />
+                <input type="password" name='password' id='' />
+                <br />
+                <input type="submit" value="Register" />
+            </form>
         </div>
     );
 };
