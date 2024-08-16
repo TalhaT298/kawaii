eec06181ab11f315d14309a610b0bf2a4f54c21f
Talha <mdtalha2008@gmail.com>
2024-08-17 02:11:29 +0600

    fix this value in name

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -37,6 +37,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    console.log(email, password);
+  };
+
   return (
     <div>
       <Link to="/login"></Link>
@@ -51,13 +59,14 @@ const Login = () => {
             </p>
           </div>
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <form className="card-body">
+            <form onSubmit={handleLogin} className="card-body">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
                 <input
                   type="email"
+                  name="email"  // Added name attribute
                   placeholder="email"
                   className="input input-bordered"
                   required
@@ -69,6 +78,7 @@ const Login = () => {
                 </label>
                 <input
                   type="password"
+                  name="password"  // Added name attribute
                   placeholder="password"
                   className="input input-bordered"
                   required
